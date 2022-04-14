d01c3991afc49ba2f03f75d78348a4c40e1c7e43
William <williamwijaya.mail@gmail.com>
2022-04-14 04:20:31 +0000

    pair programming W02D3

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// PAIR PROGRAM: WORDSEARCH - NICOLAS REIMER - WILLIAM WIJAYA - APRIL 13/22
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const columnObject = {};
+
+  for (let row = 0; row < letters.length; row++) {
+
+    for (let col = 0; col < letters[row].length; col++) {
+            
+      const letter = letters[row][col];
+      const key = "Col" + col;
+
+      if (!columnObject[key]) {
+        columnObject[key] = letter;
+      } else {
+        columnObject[key] += letter;
+      }
+    }
+  }
+  let outputArr = Object.values(columnObject);
+
+  for (let i = 0; i < outputArr.length; i++) {
+            
+    if (outputArr[i] === word) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+
